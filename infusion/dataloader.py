17746b064e9b7511b073c3267d16de179edcdc7e
J Rosser <jennifer.rosser@kellogg.ox.ac.uk>
2025-11-11 17:25:11 +0000

    making cifar partial retrain

--- infusion/dataloader.py
+++ infusion/dataloader.py
@@ -8,9 +8,11 @@ class FixedPermutationSampler(Sampler[int]):
     Yields a single random permutation (seeded) of indices repeatedly.
     That means every epoch sees the same shuffled order.
     """
-    def __init__(self, data_source: Dataset):
+    def __init__(self, data_source: Dataset, seed: int = None):
         self.data_source = data_source
         g = torch.Generator()
+        if seed is not None:
+            g.manual_seed(seed)
         self._perm = torch.randperm(len(self.data_source), generator=g).tolist()
 
     def __iter__(self):
@@ -22,12 +24,12 @@ class FixedPermutationSampler(Sampler[int]):
 
 
 
-def get_dataloader(ds, batch_size):
+def get_dataloader(ds, batch_size, seed=None):
 
     train_dl = DataLoader(
         ds,
         batch_size=batch_size,
-        sampler=FixedPermutationSampler(ds),
+        sampler=FixedPermutationSampler(ds, seed=seed),
         shuffle=False,
         num_workers=4,
         pin_memory=True,
